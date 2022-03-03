32e5fd7de0aa61cf936a060fb388ed78af8ffaad
Vašek <wasekva@gmail.com>
2022-03-03 12:50:44 +0100

    Created new project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.vut_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.vut_project to javafx.fxml;
+    exports com.example.vut_project;
+}
