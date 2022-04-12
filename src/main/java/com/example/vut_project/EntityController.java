cec20e994433adb589a2d05d24327bb3dffd786e
Vašek <wasekva@gmail.com>
2022-04-12 19:09:24 +0200

    Added attributes

--- src/main/java/com/example/vut_project/EntityController.java
+++ src/main/java/com/example/vut_project/EntityController.java
@@ -6,6 +6,7 @@ import com.example.vut_project.controller.ClassController;
 import com.example.vut_project.controller.ClassDiagramController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -13,6 +14,7 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -45,6 +47,8 @@ public class EntityController extends VBox {
         fxmlLoader.load();
         ClassController new_class = diagram.createClass(new_name);
         classNameTextField.setText(new_class.getName());
+        entityAttributeView.setEditable(true);
+        entityAttributeView.setCellFactory(TextFieldListCell.forListView());
     }
 
     public EntityController(ClassController class_name) throws IOException { //diagram added loaded file
@@ -57,12 +61,16 @@ public class EntityController extends VBox {
             observableListOfAttributes.add(attribute.getName());
         }
         this.entityAttributeView.setItems(observableListOfAttributes);
+        entityAttributeView.setEditable(true);
+        entityAttributeView.setCellFactory(TextFieldListCell.forListView());
 
     }
     public void onClassDiagramClick(MouseEvent mouseEvent){
         System.out.println(this.classNameTextField.getText());
     }
-    public void onAddAttributeClick(MouseEvent event){
+
+    public void onAddAttributeClick(ActionEvent event){
+        this.entityAttributeView.getItems().add("new_argument");
         System.out.println("CLICKED ON ADD ATTRIBUTE");
     }
 
