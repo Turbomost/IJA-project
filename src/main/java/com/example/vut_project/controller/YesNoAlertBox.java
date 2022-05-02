bf37bb9bb3a0f0940170778983ab1cb250878e38
pindo696 <pindo696@outlook.com>
2022-05-02 19:00:20 +0200

    YesNoAlertBox
    Not tested

---
+++ src/main/java/com/example/vut_project/controller/YesNoAlertBox.java
@@ -0,0 +1,35 @@
+package com.example.vut_project.controller;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class YesNoAlertBox {
+    public static int display(String title, String message, String button1_message, String button2_message) {
+        final int[] choice = {-1};
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(350);
+        window.setMinHeight(200);
+        Label label = new Label();
+        label.setText(message);
+        Button yesButton = new Button(button1_message);
+        yesButton.setOnAction(event ->{choice[0] = 1; window.close();});
+        Button noButton = new Button(button2_message);
+        noButton.setOnAction(event -> {choice[0] = 0; window.close();});
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, yesButton, noButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+        return (int) choice[0];
+    }
+}
