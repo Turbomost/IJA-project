bb3673750554cf03cbd00c5ff3c2a7bf32fb473e
pindo696 <pindo696@outlook.com>
2022-05-05 23:50:26 +0200

    Cardinality layout, Cardinality types

--- src/main/java/com/example/vut_project/controller/EditConstraintPopUp.java
+++ src/main/java/com/example/vut_project/controller/EditConstraintPopUp.java
@@ -11,7 +11,11 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -31,61 +35,73 @@ public class EditConstraintPopUp {
         constraintNameTextField.setPromptText("Constraint name");
         constraintNameTextField.setMaxHeight(50);
         constraintNameTextField.setMaxWidth(200);
+        constraintNameTextField.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        constraintNameTextField.setPrefWidth(Region.USE_COMPUTED_SIZE);
 
 
         HBox hboxDescription = new HBox();
         Text description1 = new Text("Cardinality");
         Text description2 = new Text("Type");
         Text description3 = new Text("Cardinality");
-        hboxDescription.setMargin(description1, new Insets(10, 32, 0, 10));
-        hboxDescription.setMargin(description2, new Insets(10, 8, 0, 10));
-        hboxDescription.setMargin(description3, new Insets(10, 8, 0, 10));
+        hboxDescription.setMargin(description1, new Insets(10, 10, 0, 10));
+        hboxDescription.setMargin(description2, new Insets(10, 50, 0, 50));
+        hboxDescription.setMargin(description3, new Insets(10, 10, 0, 10));
         hboxDescription.getChildren().addAll(description1, description2, description3);
-
+        hboxDescription.setAlignment(Pos.CENTER);
 
         HBox hboxChoice = new HBox();
 
         final String[] choiceBoxLeftResult = {null};
-        String constraintLeftRoll[] = {"1..n", "0..n", "0..1"};
+        choiceBoxLeftResult[0] = reference.left_cardinality;
+        String constraintLeftRoll[] = {"1..n", "0..n", "0..1", "1..1"};
         ChoiceBox constraintChoiceBoxLeft = new ChoiceBox(FXCollections.observableArrayList(constraintLeftRoll));
+        constraintChoiceBoxLeft.setValue(choiceBoxLeftResult[0]);
         constraintChoiceBoxLeft.setAccessibleText("Constraint Left");
         // add a listener
         constraintChoiceBoxLeft.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             // if the item of the list is changed
             public void changed(ObservableValue ov, Number value, Number new_value) {
-                choiceBoxLeftResult[0] = (String) constraintLeftRoll[new_value.intValue()];
+                choiceBoxLeftResult[0] = constraintLeftRoll[new_value.intValue()];
                 System.out.println("LEFT CARDINALITY: " + choiceBoxLeftResult[0]);
             }
         });
 
 
         final String[] choiceBoxDataTypeResult = {null};
-        String constraintDataType[] = {"int", "bool", "string", "nil"}; // TODO ake to boli xd si nepamatam sad noises ... tak zatial datove typy
+        choiceBoxDataTypeResult[0] = reference.getLineType();
+        String constraintDataType[] = {BoundLine.BoundLineAssociation(), BoundLine.BoundLineAggregation(), BoundLine.BoundLineComposition(), BoundLine.BoundLineGeneralization()}; // TODO ake to boli xd si nepamatam sad noises ... tak zatial datove typy
         ChoiceBox constraintChoiceBoxMiddle = new ChoiceBox(FXCollections.observableArrayList(constraintDataType));
+        constraintChoiceBoxMiddle.setValue(choiceBoxDataTypeResult[0]);
         constraintChoiceBoxLeft.setAccessibleText("Constraint Data Type");
         // add a listener
         constraintChoiceBoxLeft.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             // if the item of the list is changed
             public void changed(ObservableValue ov, Number value, Number new_value) {
-                choiceBoxDataTypeResult[0] = (String) constraintDataType[new_value.intValue()];
+                choiceBoxDataTypeResult[0] = constraintDataType[new_value.intValue()];
                 System.out.println("LEFT CARDINALITY: " + choiceBoxDataTypeResult[0]);
             }
         });
 
 
         final String[] choiceBoxRightResult = {null};
-        String constraintRightRoll[] = {"1..n", "0..n", "0..1"};
+        choiceBoxRightResult[0] = reference.right_cardinality;
+        String constraintRightRoll[] = {"1..n", "0..n", "0..1", "1..1"};
         ChoiceBox constraintChoiceBoxRight = new ChoiceBox(FXCollections.observableArrayList(constraintRightRoll));
+        constraintChoiceBoxRight.setValue(choiceBoxRightResult[0]);
         constraintChoiceBoxRight.setAccessibleText("Constraint Right");
         // add a listener
         constraintChoiceBoxRight.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             // if the item of the list is changed
             public void changed(ObservableValue ov, Number value, Number new_value) {
-                choiceBoxRightResult[0] = (String) constraintRightRoll[new_value.intValue()];
+                choiceBoxRightResult[0] = constraintRightRoll[new_value.intValue()];
                 System.out.println("LEFT CARDINALITY: " + choiceBoxRightResult[0]);
             }
         });
 
+        hboxDescription.setMargin(constraintChoiceBoxLeft, new Insets(10, 10, 10, 10));
+        hboxDescription.setMargin(constraintChoiceBoxMiddle, new Insets(10, 10, 10, 10));
+        hboxDescription.setMargin(constraintChoiceBoxRight, new Insets(10, 10, 10, 10));
+        hboxChoice.setAlignment(Pos.CENTER);
         hboxChoice.getChildren().addAll(constraintChoiceBoxLeft, constraintChoiceBoxMiddle, constraintChoiceBoxRight);
 
         editButton.setOnAction(e -> {
@@ -98,8 +114,14 @@ public class EditConstraintPopUp {
             editConstraintPopUpWindow.close();
         });
 
+        HBox hboxLabel = new HBox();
+        hboxLabel.setMargin(label, new Insets(10, 10, 20, 10));
+        label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        hboxLabel.getChildren().add(label);
+        hboxLabel.setAlignment(Pos.CENTER);
 
-        layout.getChildren().addAll(label, constraintNameTextField, hboxDescription, hboxChoice, editButton);
+
+        layout.getChildren().addAll(hboxLabel, constraintNameTextField, hboxDescription, hboxChoice, editButton);
         layout.setAlignment(Pos.CENTER);
 
         editConstraintPopUpWindow.initModality(Modality.APPLICATION_MODAL);
@@ -109,12 +131,5 @@ public class EditConstraintPopUp {
         editConstraintPopUpWindow.setScene(addAttributeScene);
 
         editConstraintPopUpWindow.showAndWait();
-        String[] result = {null, null, null, null};
-
-        result[0] = constraintName[0];
-        result[1] = choiceBoxLeftResult[0];
-        result[2] = choiceBoxDataTypeResult[0];
-        result[3] = choiceBoxRightResult[0];
-
     }
 }
